55c3b224efbfc23370242363bacb75aab604d459
XPRISTO <XPRISTO@DESKTOP-3EGSATI>
2024-07-11 14:30:53 +0300

    Add project files.

---
+++ Lab task 1/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+//--------------------------------------------------task 3
+    //class student
+    //{
+
+    //    string name = "Null";
+    //    int age = 0;
+    //    int ssn = 0;
+    //    string phone = "+++++++";
+    //    public void GetData(string Name , int age, int ssn , string phone)
+    //    {
+    //        this.name = Name;
+    //        this.age = age;
+    //        this.ssn = ssn;
+    //        this.phone = phone;
+    //    }
+    //    public void print()
+    //    {
+    //        Console.WriteLine($"{name}-{age}-{ssn}-{phone}");
+    //    }
+    //}
+    class program {
+
+
+        static void Main()
+        {
+            //Console.WriteLine();
+            //student st1 = new student();
+            //st1.GetData("seif",10,22,"0111111111");
+            //st1.print();
+            //student st2 = new student();
+            //st2.print();
+
+            //------------------------------------------------task 2
+            //Console.WriteLine("Enter your ID");
+            //int ID = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter your Name");
+            //string Name = (Console.ReadLine());
+            //int Age;
+            //do
+            //{
+            //    Console.WriteLine("Enter your Age: ");
+            //    Age = int.Parse(Console.ReadLine());
+
+
+            //} while (Age < 0);
+
+            //Console.WriteLine($"Your ID is: {ID}");
+            //Console.WriteLine($"Your Name is: {Name}");
+            //Console.WriteLine($"Your Age is: {Age}");
+
+            //---------------------------------------------------task 1
+            //Console.WriteLine("Enter a number");
+            //int number = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++) 
+            //{
+            //    Console.WriteLine($"{number} * {i} = {number*i}");
+            //}
+        }
+    } 
+
+}
